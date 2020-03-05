eb657b2281cdfd6638647542ed94b3381011db72
= <=>
2020-03-04 21:11:35 -0500

    Update and refined code.

--- 05-Stock-Ticker/stock_index.py
+++ 05-Stock-Ticker/stock_index.py
@@ -1,9 +1,9 @@
 ######################################################################
 #
 #   Filename      : stock_ticker.py
-#   Description   : Display stock-index prices from yahoo finance
+#   Description   : Display stock/index prices from yahoo finance
 #   Author        : Israel Dryer
-#   Modified      : 2019-09-28
+#   Modified      : 2020-03-04
 #
 ######################################################################
 #
@@ -19,63 +19,86 @@ from time import sleep
 import requests
 import lxml
 
-# add to this list or replace with any stock you want on Yahoo! Finance
+# add to this list or replace with any stock/index you want on Yahoo! Finance
 index = {'^GSPC':'S&P 500', '^DJI':'DOW 30', '^IXIC':'NASDAQ', '^RUT':'Russell 2000'} 
 url = 'https://finance.yahoo.com/quote/{}?p={}'
 
-def get_html_data(index_list, url):
-    # lookup index prices and save to dictionary
-    html_data = {}
-    for k,v in index.items():
-        new_url = url.format(k, k)
+def get_curr_prices(index_list, url):
+    """ Create a dictionary containing current index prices """
+    
+    curr_prices = {}
+    
+    for symbol, name in index.items():
+        
+        # create url with index symbol
+        new_url = url.format(symbol, symbol)
+        
+        # get the index html data from yahoo finance
         r = requests.get(new_url)
-        # {stock name: stock price}
-        html_data[v] = r.text
-    return html_data
+        
+        # save name and price information in dictionary: {name: price}
+        curr_prices[name] = r.text
+        
+    return curr_prices
+
 
 def parse_html(html_data):
-    # parse the html data and extract relevant parts for printing
-    stock_ticker_data = []
+    """ Parse html data and extract relevant sections """
+    
+    ticker_data = []
+    
     for stock, html in html_data.items():
+        
+        # create BS4 object for parsing html
         soup = BeautifulSoup(html, 'lxml')
-        # current stock price
+        
+        # current index price
         price = soup.find('span',{'class':"Trsdu(0.3s)"})
+        
         # change by amount and percent
         change_amt, change_pct = price.find_next_sibling().span.text.split(' ')
+        
         # market close notice
         status, last_update = soup.find('div',{'id':'quote-market-notice'}).span.text.split(':  ')
+        
         # save ticker data to list
-        stock_ticker_data.append((stock, price.text.strip(), change_amt, change_pct, last_update))
-    return stock_ticker_data
+        ticker_data.append((stock, price.text.strip(), change_amt, change_pct, last_update))
+        
+    return ticker_data
+
+
 
 def formatter(data_row):
-    # format the stock data and return 2 lines for printing to lcd
+    """ format the stock data and return 2 lines for printing to lcd """
     stock, price, change_amt, change_pct, last_update = data_row
     line1 = stock
     line2 = '{} {}'.format(price, change_amt)
     return line1, line2
 
-def loop():
 
+def loop():
+    """ The main program loop """
     while True:
+        
         # retrieve and parse stock-index prices
-        html_data = get_html_data(index, url)
-        stock_ticker_data = parse_html(html_data)
+        curr_prices = get_curr_prices(index, url)
+        ticker_data = parse_html(curr_prices)
 
         # write message
-        for stock in stock_ticker_data:
-            line1, line2 = formatter(stock)
+        for item in ticker_data:
+            line1, line2 = formatter(item)
 
             lcd.home()
             lcd.clear()
             lcd.write_string(line1 + '\n\r')
             lcd.write_string(line2)
-            sleep(5)
+            sleep(5)  # delay for 5 seconds between indexes
 
-# create LCD
-lcd = CharLCD(i2c_expander='PCF8574', address=0x27)
 
 if __name__ == '__main__':
+
+    # Initialize LCD object
+    lcd = CharLCD(i2c_expander='PCF8574', address=0x27)
     lcd.clear()
     lcd.home()
     lcd.write_string('Loading...')
@@ -83,6 +106,6 @@ if __name__ == '__main__':
     try:
         loop()
     except:
-        KeyboardInterrupt
+        KeyboardInterrupt  # CTRL+C
         lcd.clear()
         lcd.close()
